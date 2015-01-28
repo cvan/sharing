599c3c1f4d1ec8c4fc6763ec44fa76a197f14f39
Michael Henretty <michael.henretty@gmail.com>
2015-01-27 18:02:48 -0800

    fix relative component import

--- app/js/globals.js
+++ app/js/globals.js
@@ -1,6 +1,8 @@
 window.COMPONENTS_BASE_URL = './components/';
 require.config({
 	paths: {
-		'gaia-component': 'components/gaia-component/gaia-component'
+		'gaia-component': 'gaia-component/gaia-component',
+		'drag': 'drag/drag',
+		'gaia-icons': 'gaia-icons/gaia-icons'
 	}
 });
--- dist/app/js/globals.js
+++ dist/app/js/globals.js
@@ -4,7 +4,9 @@ define(["exports"], function (exports) {
   window.COMPONENTS_BASE_URL = "./components/";
   require.config({
     paths: {
-      "gaia-component": "components/gaia-component/gaia-component"
+      "gaia-component": "gaia-component/gaia-component",
+      drag: "drag/drag",
+      "gaia-icons": "gaia-icons/gaia-icons"
     }
   });
 });
