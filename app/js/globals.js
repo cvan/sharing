e3f3411a62f138423f9ff55f227291d779ab1086
Michael Henretty <michael.henretty@gmail.com>
2015-01-27 17:57:50 -0800

    use path config for gaia-component

--- app/js/globals.js
+++ app/js/globals.js
@@ -1,1 +1,6 @@
 window.COMPONENTS_BASE_URL = './components/';
+require.config({
+	paths: {
+		'gaia-component': 'components/gaia-component/gaia-component'
+	}
+});
--- dist/app/js/globals.js
+++ dist/app/js/globals.js
@@ -2,4 +2,9 @@ define(["exports"], function (exports) {
   "use strict";
 
   window.COMPONENTS_BASE_URL = "./components/";
+  require.config({
+    paths: {
+      "gaia-component": "components/gaia-component/gaia-component"
+    }
+  });
 });
